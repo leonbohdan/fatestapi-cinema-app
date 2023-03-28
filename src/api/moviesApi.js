5f383f762cff78d6545f2f98563c9352d59773b6
bohdan leonets <bohdan@mytowerapp.com>
2023-03-28 11:07:13 +0300

    add pinia store

--- src/api/moviesApi.js
+++ src/api/moviesApi.js
@@ -5,6 +5,5 @@ export const getMovies = (params) => apiClient.get('/movies', { params });
 export const getMovieShows = (params) => apiClient.get('/movieShows', { params });
 
 export const checkFreePlaces = (params) => apiClient.get('/showPlaces', { params });
-// ?movie_id={id}&daytime={daytime}&showdate={showdate}
 
 export const bookPlace = (data) => apiClient.post('/bookPlace', data);
